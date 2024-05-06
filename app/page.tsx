946feb3b0487706b2f8e099514e09ddf209c0464
Nghiep <me@nghiep.dev>
2024-05-06 21:30:46 +0700

    Add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
 import clsx from 'clsx';
+import Marquee from 'react-fast-marquee';
 
 import Social from './social';
 
 import cloud1Img from './cloud-1.png';
+import telegramImg from './telegram.svg';
+import twitterImg from './twitter.svg';
 
 export default function Home() {
   return (
-    <main className='relative mx-auto min-h-screen max-w-[1440px]'>
+    <main className='relative mx-auto min-h-screen max-w-[1440px] overflow-hidden'>
       <Image
         src={cloud1Img}
         alt=''
@@ -60,6 +63,69 @@ export default function Home() {
             alt=''
             className='absolute right-[12.5rem] top-[-17rem]'
           />
+        </div>
+      </div>
+
+      <div className='relative z-[-1] ml-[-100px] mt-[30px] w-[150%] origin-top-left rotate-[-24.1deg] bg-primary py-10'>
+        <Marquee autoFill className='overflow-hidden'>
+          <span className='inline-block px-10 text-[64px] text-white'>
+            $datekni
+          </span>
+        </Marquee>
+      </div>
+
+      <div className='-mt-80'>
+        <div className='flex justify-center'>
+          <div className='self-end max-lg:hidden'>
+            <img src='/soldier-6.gif' alt='' className='w-[512px]' />
+          </div>
+          <div className='flex shrink-0 flex-col items-center'>
+            <img src='/soldier-5.gif' alt='' className='w-[512px]' />
+            <hgroup className='text-center'>
+              <h2 className='font-knight text-9xl text-primary'>Tokenomics</h2>
+              <div className='mt-5 text-5xl font-bold'>Supply: 400.000.000</div>
+              <div className='mt-5 text-[32px]'>
+                No presale | No farming | No secret algo <br /> No claim process
+                | No paid marketing <br /> No team allocation | No favoritism to
+                whales
+              </div>
+            </hgroup>
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-32'>
+        <div className='flex'>
+          <div className='overflow-hidden max-lg:hidden'>
+            <img
+              src='/soldier-7.gif'
+              alt=''
+              className='-mb-16 w-[664px] self-end'
+            />
+          </div>
+          <div className='relative shrink-0'>
+            <img
+              src='/soldier-8.gif'
+              alt=''
+              className='absolute -bottom-5 left-0 z-[-1] w-[326px]'
+            />
+            <img
+              src='/soldier-9.gif'
+              alt=''
+              className='absolute -bottom-16 -right-72 z-[-1] w-[410px]'
+            />
+            <h2 className='text-center font-knight text-9xl text-primary'>
+              Community
+            </h2>
+            <div className='mt-7 flex justify-center gap-x-10'>
+              <a href={process.env.NEXT_PUBLIC_TELEGRAM_URL} target='_telegram'>
+                <Image src={telegramImg} alt='' className='size-[88px]' />
+              </a>
+              <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target='_twitter'>
+                <Image src={twitterImg} alt='' className='size-[88px]' />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </main>
